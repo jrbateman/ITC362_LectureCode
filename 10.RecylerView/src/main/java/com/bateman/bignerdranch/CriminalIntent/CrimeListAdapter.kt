95905ac80cf0ddee114006a171e79704e9d2b1f0
JRB <jrbatemanjr@gmail.com>
2022-12-15 02:47:41 -0600

    10 added

---
+++ 10.RecylerView/src/main/java/com/bateman/bignerdranch/CriminalIntent/CrimeListAdapter.kt
@@ -0,0 +1,44 @@
+package com.bateman.bignerdranch.CriminalIntent
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bateman.bignerdranch.CriminalIntent.databinding.ItemListCrimeBinding
+
+class CrimeHolder(
+    private val binding: ItemListCrimeBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime) {
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = crime.date.toString()
+
+        binding.root.setOnClickListener {
+            Toast.makeText(
+                binding.root.context,
+                "${crime.title} clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
+
+class CrimeListAdapter(
+    private val crimes: List<Crime>
+) : RecyclerView.Adapter<CrimeHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CrimeHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemListCrimeBinding.inflate(inflater, parent, false)
+        return CrimeHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+        val crime = crimes[position]
+        holder.bind(crime)
+    }
+
+    override fun getItemCount() = crimes.size
+}
