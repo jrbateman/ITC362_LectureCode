d8f7813ea1ed7a3bcf461826f0a0632b0ea053c9
jrbateman <jrbatemanjr@gmail.com>
2025-07-20 13:14:52 -0500

    Added PixelAPIView

--- 20_makingnetworkrequestspexel/src/main/java/com/bateman/makingnetworkrequestspexel/api/PexelResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.bateman.makingnetworkrequestspexel.api
-
-import com.squareup.moshi.JsonClass
-
-
-@JsonClass(generateAdapter = true)
-data class PexelResponse(
-    val photos: PhotoResponse
-)
